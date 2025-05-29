f8efe702f60445d5cb61c1a6d1efee52c11055bf
AswinJ1 <aswinjhome@gmail.com>
2025-05-29 23:18:37 +0530

    changed code to prevent route issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "@/components/Navbar"; 
 import PageTransition from "@/components/PageTransition";
 import Index from "./pages/Index";
@@ -18,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Navbar />
         <Routes>
           <Route 
@@ -37,7 +36,7 @@ const App = () => (
               </PageTransition>
             } 
           />
-           <Route 
+          <Route 
             path="/teammembers" 
             element={
               <PageTransition>
@@ -45,8 +44,8 @@ const App = () => (
               </PageTransition>
             } 
           />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
